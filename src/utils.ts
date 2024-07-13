045ae10b656064f416fda5c305f7adc7b8748daf
Nathan <nathan.globalsense@gmail.com>
2024-07-13 15:02:26 +0800

    Initial commit

---
+++ src/utils.ts
@@ -0,0 +1,15 @@
+//Format the Date to a string
+function formatDate(date: Date): string {
+    const options: Intl.DateTimeFormatOptions = {year:'numeric', month:'long', day:'numeric'};
+    return new Date(date).toLocaleDateString(undefined, options)
+  }
+//Capitilize the firs tletter
+  function capitalize(str: string): string { 
+    if(typeof str !== 'string' || str.length ===  0) {
+      return str;
+
+    }
+    return str.charAt(0).toUpperCase()  + str.slice(1);
+  }
+
+  export {formatDate , capitalize}
